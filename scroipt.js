8f0fa53fc7b02d8ba49f760805db6b9a01c959c9
ivan <ivanwolyniec@gmail.com>
2019-10-26 22:30:17 -0400

    minor changes

--- scroipt.js
+++ scroipt.js
@@ -1,10 +1,31 @@
 // SETUP FOR CARD GAME, FIND ALL CARDS
 let fronts = document.getElementsByClassName("front");
 let backs = document.getElementsByClassName("back");
+// true for front false for back
+let facing = [];
+// deck content array,
+// 0 = ★
+// 1 = ☾
+// 2 = ⌇
+// 3 = ■
+// 4 = ○
+// 5 = +
+let deck = [0, 1, 2, 3, 4, 5];
+
+function dex(neb) {
+  if (neb === 0) return `★`;
+  else if (neb === 1) return `☾`;
+  else if (neb === 2) return `⌇`;
+  else if (neb === 3) return `■`;
+  else if (neb === 4) return `○`;
+  else return `+`;
+}
 
 // ASSIGN EVENTLISTENERS TO CARD FRONTS AND FLIP CARDS
 for (let i = 0; i < fronts.length; i++) {
-  backs[i].style.transform = "rotateY(90deg)";
+  fronts[i].style.transform = "rotateY(90deg)";
+  facing.push(false);
+  fronts[i].innerHTML = dex(deck[i]);
   fronts[i].addEventListener("click", _ => {
     fronts[i].style.transition = "transform 0.5s";
     fronts[i].style.transform = "rotateY(-90deg)";
