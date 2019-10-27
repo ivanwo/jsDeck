35c8f49b78cd0a157ef0e851e1c5677085b021f1
ivan <ivanwolyniec@gmail.com>
2019-10-27 09:40:55 -0400

    changes

--- scroipt.js
+++ scroipt.js
@@ -11,20 +11,46 @@ let facing = [];
 // 4 = ○
 // 5 = +
 let deck = [0, 1, 2, 3, 4, 5];
+// psychic goal variable
+let goal = Math.floor(Math.random() * 6);
 
 function dex(neb) {
-  if (neb === 0) return `★`;
-  else if (neb === 1) return `☾`;
-  else if (neb === 2) return `⌇`;
-  else if (neb === 3) return `■`;
-  else if (neb === 4) return `○`;
-  else return `+`;
+  switch (neb) {
+    case 0:
+      return `★`;
+    case 1:
+      return `☾`;
+    case 2:
+      return `⌇`;
+    case 3:
+      return `■`;
+    case 4:
+      return `○`;
+    case 5:
+      return `+`;
+    case `★`:
+      return 0;
+    case `☾`:
+      return 1;
+    case `⌇`:
+      return 2;
+    case `■`:
+      return 3;
+    case `○`:
+      return 4;
+    case `+`:
+      return 5;
+    default:
+      return `f`;
+  }
 }
 
+shuffler(deck);
 // ASSIGN EVENTLISTENERS TO CARD FRONTS AND FLIP CARDS
 for (let i = 0; i < fronts.length; i++) {
-  fronts[i].style.transform = "rotateY(90deg)";
+  backs[i].style.transform = "rotateY(90deg)";
   facing.push(false);
+  if (deck[i] === goal) document.getElementById("res").innerHTML = "success";
   fronts[i].innerHTML = dex(deck[i]);
   fronts[i].addEventListener("click", _ => {
     fronts[i].style.transition = "transform 0.5s";
@@ -48,4 +74,27 @@ for (let i = 0; i < backs.length; i++) {
   });
 }
 
+document.getElementById("sym").addEventListener("click", _ => {
+  gameOn();
+});
+function gameOn() {
+  goal = Math.floor(Math.random() * 6);
+  document.getElementById("sym").innerHTML = dex(goal);
+  shuffler(deck);
+  reset();
+}
+function shuffler(neb) {
+  neb.sort(_ => Math.random() - 0.5);
+}
+function reset() {
+  for (let i = 0; i < deck.length; i++) {
+    backs[i].style.transform = "rotateY(-90deg)";
+    fronts[i].style.transform = "rotateY(0deg)";
+
+    fronts[i].innerHTML = dex(deck[i]);
+  }
+  document.getElementById("res").innerHTML = "";
+}
+
 // GAME BIT
+gameOn();
